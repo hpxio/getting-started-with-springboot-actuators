05e9ad9527d93991bdcc3d2c1718ad2dc9379f13
Harsh Poddar <harsh.poddar@ticketmaster.com>
2019-08-19 16:11:20 +0530

    App-Log::Init-Project

---
+++ src/main/java/com/app/rc/gettingStartedWithSpringBootActuators/GettingStartedWithSpringBootActuatorsApplication.java
@@ -0,0 +1,13 @@
+package com.app.rc.gettingStartedWithSpringBootActuators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithSpringBootActuatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedWithSpringBootActuatorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/rc/gettingStartedWithSpringBootActuators/GettingStartedWithSpringBootActuatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.rc.gettingStartedWithSpringBootActuators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWithSpringBootActuatorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
